0227ae7478d598db4e836ba020d7a68182c193c2
husky-parul <promptparul@gmail.com>
2018-03-16 10:37:27 -0400

    check hello world

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ import os
 from flask import Flask
 from flask import request
 from flask import render_template
+from kubernetes import client as k_client
+from openshift import client as o_client
+import yaml
+import json
+import os
 
 
 
@@ -19,7 +24,60 @@ def login():
     if request.method == 'POST':
         content = request.json
         print content['mytext']
-        
+        name='conclavepy'
+        # image='docker.io/singhp11/pyspark-python3'
+        image='singhp11/python3-hello-world'
+
+        d_job = {
+            "apiVersion": "batch/v1",
+            "kind": "Job",
+            "metadata": {
+                "name": name
+            },
+            "spec": {
+                "parallelism": 1,
+                "completions": 1,
+                "activeDeadlineSeconds": 3600,
+                "template": {
+                    "metadata": {
+                        "name": name
+                    },
+                    "spec": {
+                        "restartPolicy": "Never",
+                        "containers": [
+                            {
+                                "name": name,
+                                "image": image,
+
+                                # "volumeMounts": [
+                                #     {
+                                #         "name": "config-volume",
+                                #         "mountPath": "/etc/config"
+                                #     }
+                                # ]
+                            }
+                        ]
+                    }
+                }
+            }
+        }
+        # d_job['spec']['template']['spec']['volumes'] = [
+        #     {
+        #         "name": "config-volume",
+        #         "configMap":[
+        #             {
+        #                 "name": "special-config"
+        #             }
+        #         ]
+        #     }
+        # ]
+
+        kube_client = k_client.CoreV1Api()
+        kube_v1_batch_client = k_client.BatchV1Api()
+        project = os.environ.get('OPENSHIFTMGR_PROJECT') or 'conclave-web'
+        job = kube_v1_batch_client.create_namespaced_job(namespace=project, body=d_job)
+
+
         return render_template('submit.html')
 
 
