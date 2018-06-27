9e2eeab025596b76576e63a3143f68174ddfa85d
Ben <bengetch@gmail.com>
2018-06-27 16:12:13 -0400

    remove openshift client commands

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
+import time
+import logging
 import os
-import time
-import logging
 
 from flask import Flask
 from flask import request
@@ -54,100 +54,8 @@ def submit():
     TODO: how to bind service account credentials to this?
     """
 
-    k_cfg = k_client.Configuration()
-    k_cfg.api_key['authorization'] = open('/var/run/secrets/kubernetes.io/serviceaccount/token').read()
-
-    # o_config.load_kube_config(config_file='/tmp/.kube/config')
-    api = k_client.CoreV1Api(k_client.ApiClient(k_cfg))
+    kube_client = k_client.CoreV1Api()
     kube_batch_client = k_client.BatchV1Api()
-
-    timestamp = str(int(round(time.time() * 1000)))
-
-    if request.method == 'POST':
-
-        jsondata = request.get_json(force=True)
-        config_data = jsondata['config']
-        protocol = ["{}\n".format(item) for item in jsondata['protocol']]
-
-        data = \
-            {
-                "protocol.py":  protocol,
-                "config_data": config_data
-            }
-
-        configmap_name = ''.join(['conclaveweb', '-', timestamp])
-        configmap_metadata = k_client.V1ObjectMeta(name=configmap_name)
-        configmap_body = k_client.V1ConfigMap(data=data, metadata=configmap_metadata)
-
-        try:
-            api_response = api.create_namespaced_config_map('cici', configmap_body, pretty='true')
-            app.logger.info("Namespace created successfully with response {}\n".format(api_response))
-        except ApiException as e:
-            app.logger.error("Error creating config map: {}\n".format(e))
-
-        name = ''.join(['conclave-web-hw', '-', timestamp])
-        image = 'docker.io/singhp11/python3-hello-world'
-
-        import os
-import time
-import logging
-
-from flask import Flask
-from flask import request
-from flask import render_template
-from flask import jsonify
-from openshift import config as o_config
-from openshift import client as o_client
-from kubernetes import client as k_client
-from kubernetes.client.rest import ApiException
-from flask_cors import CORS
-
-app = Flask(__name__, static_folder="./dist/static", template_folder="./dist")
-
-# CORS to allow status calls from backend to frontend
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
-
-
-if __name__ != "__main__":
-    gunicorn_logger = logging.getLogger("gunicorn.error")
-    app.logger.handlers = gunicorn_logger.handlers
-    app.logger.setLevel(gunicorn_logger.level)
-
-
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    return render_template("index.html")
-
-
-@app.route('/api/job_status')
-def job_status(status=None):
-    """
-    After querying status of Conclave job, send response to frontend.
-    """
-
-    # dummy temp response
-    if status is None:
-        status = 'everything ok'
-
-    response = \
-        {
-            'status': status
-        }
-
-    return jsonify(response)
-
-
-@app.route('/api/submit', methods=['POST'])
-def submit():
-    """
-    TODO: how to bind service account credentials to this?
-    """
-
-    config=o_config.load_kube_config(config_file='/tmp/.kube/config')
-    openshift_client=o_client.OapiApi()
-    kube_client=k_client.CoreV1Api()
-    kube_batch_client=k_client.BatchV1Api()
 
     timestamp = str(int(round(time.time() * 1000)))
 
@@ -201,7 +109,7 @@ def submit():
 
                                         {
                                             "name": "KUBECFG_PATH",
-                                            "value": "/tmp/.kube/config"
+                                            "value": "/tmp/.kube/token"
                                         },
                                         {
                                             "name": "OPENSHIFTMGR_PROJECT",
