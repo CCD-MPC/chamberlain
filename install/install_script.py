8eb5e8eb84fc893284b644500a47ffca608c8051
Ben <bengetch@gmail.com>
2018-12-08 15:33:39 -0500

    default args false

--- install/install_script.py
+++ install/install_script.py
@@ -6,9 +6,9 @@ from install import ConclaveWebInstaller
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Install C2D on OpenShift")
-    parser.add_argument("--swift", type=bool, default=True, required=False)
-    parser.add_argument("--dv", type=bool, default=True, required=False)
-    parser.add_argument("--vol", type=bool, default=True, required=False)
+    parser.add_argument("--swift", type=bool, default=False, required=False)
+    parser.add_argument("--dv", type=bool, default=False, required=False)
+    parser.add_argument("--vol", type=bool, default=False, required=False)
 
     args = parser.parse_args()
 
