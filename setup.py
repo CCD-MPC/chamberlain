1c871c5a989e368907251bc9f97eb711cf4503a8
Ben <bengetch@gmail.com>
2019-02-25 15:54:33 -0500

    setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name             = 'conclave-web',
+    version          = '0.0.0.1',
+    packages         = find_packages(),
+    license          = 'MIT',
+    url              = 'https://github.com/cici-conclave/conclave-web',
+    description      = 'Runs Conclave jobs on the MOC',
+    long_description = open('README.md').read()
+)
