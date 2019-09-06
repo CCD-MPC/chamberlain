55c1f1b855cec319548960cad597b35d148671e1
Ben <bengetch@gmail.com>
2019-09-06 15:09:40 -0400

    fix setup script and chamberlain deploy json

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version          = '0.0.0.1',
     packages         = find_packages(),
     license          = 'MIT',
-    url              = 'https://github.com/cici-conclave/conclave-web',
+    url              = 'https://github.com/cici-conclave/chamberlain',
     description      = 'Runs Conclave jobs on the MOC',
     long_description = open('README.md').read()
 )
